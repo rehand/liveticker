498b09ca1dcd98e2d07131d9b331aabaaecf08f7
rehand <reinhard.handler@gmail.com>
2015-03-24 11:47:41 +0100

    removed manually created add team form (using autoform instead)

--- client/views/admin/adminTeams.js
+++ client/views/admin/adminTeams.js
@@ -3,28 +3,6 @@
  */
 if (Meteor.isClient) {
     Template.adminTeams.events({
-        "submit .new-team": function (event, template) {
-            event.preventDefault();
-
-            //clearErrors();
-
-            // This function is called when the new team form is submitted
-            Meteor.call("addTeam", {
-                name: event.target.name.value,
-                code: event.target.code.value
-            }, function(error) {
-                if (error) {
-                    throwError(error.reason);
-                } else {
-                    // Clear form
-                    event.target.name.value = "";
-                    event.target.code.value = "";
-                }
-            });
-
-            // Prevent default form submit
-            return false;
-        },
         "click .delete-team": function (event, template) {
             event.preventDefault();
 
