6471188f9fb4449bd82f6d3cba687e735e3a63da
rehand <reinhard.handler@gmail.com>
2015-04-24 12:05:00 +0200

    fixed issue when editing a team (cursor for images needed)

--- server/publications/ImagesPub.js
+++ server/publications/ImagesPub.js
@@ -1,7 +1,7 @@
 Meteor.publish('TeamLogo', function (teamId) {
     check(teamId, String);
     var team = Teams.findOne({code: teamId});
-    return Images.findOne(team.logo);
+    return Images.find(team.logo);
 });
 
 Meteor.publish('Images', function () {
