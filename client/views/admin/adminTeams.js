6fe1fbfe77095733f92d0ec4007e05f18eb0d411
rehand <reinhard.handler@gmail.com>
2015-06-20 11:47:57 +0200

    removed team deletion from team overview

--- client/views/admin/adminTeams.js
+++ client/views/admin/adminTeams.js
@@ -3,18 +3,6 @@
  */
 if (Meteor.isClient) {
     Template.adminTeams.events({
-        "click .delete-team": function (event, template) {
-            event.preventDefault();
-
-            // This function is called when the new team form is submitted
-            Meteor.call("deleteTeam", event.target.parentNode.value, function(error) {
-                if (error) {
-                    throwError(error.reason);
-                }
-            });
-
-            // Prevent default form submit
-            return false;
-        }
+        
     });
 }
