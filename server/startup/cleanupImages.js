3ae8436747cda88134a97e3637f727102746302d
rehand <reinhard.handler@gmail.com>
2015-06-30 19:11:10 +0200

    added unused image cleanup script

---
+++ server/startup/cleanupImages.js
@@ -0,0 +1,16 @@
+/**
+ * Created by reini on 29.06.15.
+ */
+
+Meteor.startup(function () {
+    var teamLogos = Teams.find({logo: {$ne: null}}, {fields: {logo: true}}).fetch();
+    var imageIds = [];
+
+    teamLogos.forEach(function (teamLogo) {
+        imageIds.push(teamLogo.logo);
+    });
+
+    console.log("Removing Images except IDs: " + JSON.stringify(imageIds));
+
+    Images.remove({_id: {$nin: imageIds}});
+});
