6a42402d5423aed903a3aa07b793fe0001ca768e
rehand <reinhard.handler@gmail.com>
2015-10-10 22:55:21 +0200

    implemented sticky match info

--- client/views/frontend/frontendTickerDetail.js
+++ client/views/frontend/frontendTickerDetail.js
@@ -1,6 +1,6 @@
 var timer;
 
-Template.frontendTickerDetail.created = function() {
+Template.frontendTickerDetail.created = function () {
     serverOffset = TimeSync.serverOffset();
     calcGameTime();
     timer = Meteor.setInterval(calcGameTime, 1000);
@@ -8,6 +8,13 @@ Template.frontendTickerDetail.created = function() {
     Session.setDefault(SESSION_PLAY_AUDIO, true);
 };
 
-Template.frontendTickerDetail.destroyed = function() {
+Template.frontendTickerDetail.rendered = function () {
+    setTimeout(function () {
+        $("#matchInfo").sticky({topSpacing: 0, className: 'sticky', getWidthFrom: '#errors', responsiveWidth: true});
+        $("#matchInfo").sticky('update');
+    }, 2000);
+};
+
+Template.frontendTickerDetail.destroyed = function () {
     Meteor.clearInterval(timer);
 };
