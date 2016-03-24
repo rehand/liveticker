94f1632aecacb8ddfc42c897bd9ef037c66331f2
rehand <reinhard.handler@gmail.com>
2016-03-24 14:22:28 +0100

    added serviceWorker

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+var CACHE_NAME = 'liveticker-cache-v1';
+var urlsToCache = [
+    '/images/header.jpg',
+    '/images/in.png',
+    '/images/out.png',
+    '/images/red.png',
+    '/images/yellow.png',
+    '/images/yellowred.png',
+    '/audio/hymne_sturm.mp3'
+];
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
