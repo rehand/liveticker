2d5df2beb55dceff84296b66791534051589c782
rehand <reinhard.handler@gmail.com>
2016-04-21 11:10:42 +0200

    added possibility to allow comments after the game has ended for a certain amount of minutes

--- client/views/frontend/frontendTickerDetail.js
+++ client/views/frontend/frontendTickerDetail.js
@@ -23,6 +23,26 @@ Template.registerHelper("getCommentName", function() {
     return Session.get(SESSION_COMMENT_NAME);
 });
 
+var COMMENTS_ALLOWED_AFTER_END_MILLIS = 8 * 60 * 1000;
+var commentsAllowedDep = new Deps.Dependency();
+Template.registerHelper("allowComments", function () {
+    var timeSecondHalfEnd = this.ticker.timeSecondHalfEnd;
+
+    var timeoutReached;
+    if (timeSecondHalfEnd && !(timeoutReached = (new Date().getTime() - timeSecondHalfEnd.getTime() > COMMENTS_ALLOWED_AFTER_END_MILLIS))) {
+        var timeout = COMMENTS_ALLOWED_AFTER_END_MILLIS - (new Date().getTime() - timeSecondHalfEnd.getTime());
+        if (timeout > 0) {
+            commentsAllowedDep.depend();
+
+            setTimeout(function () {
+                commentsAllowedDep.changed();
+            }, timeout);
+        }
+    }
+
+    return !timeSecondHalfEnd || !timeoutReached;
+});
+
 AutoForm.hooks({
     addTickerComment: {
         onSuccess: function (formType, newCommentName) {
